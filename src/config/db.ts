ba76f64e254adcb7ff34f2a1c806edadbd38970f
Antonio Évora <aevora@inntelia.com>
2025-04-10 19:32:06 +0200

    Variables de entorno

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,12 +4,12 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME || '',
-  process.env.DB_USER || '',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME || "",
+  process.env.DB_USER || "",
+  process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: "mysql",
     port: Number(process.env.DB_PORT),
     logging: false,
   }
